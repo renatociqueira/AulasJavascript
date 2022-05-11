22d427fbb119be2670060cd4cb527825b49321fd
renatociqueira <37406514+renatociqueira@users.noreply.github.com>
2022-05-11 16:41:08 -0300

    Aulas de Javascript

---
+++ Aula13/ambiente.js
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------------
+// Para executar o código instalar a extensão node.js Exec
+// Para utilizar a extensão apenas pressionar o F8
+// Exercicio de estrutura de repetições parte 1
+// ------------------------------------------------------------------ 
+
+var c = 1
+// enquanto
+while ( c <= 6){
+    console.log(`Passo enquanto ${c}`)
+    c++
+}
+
+// Faça enquanto
+do {
+    console.log(`Passo Faça enquanto ${c}`)
+    c++
+} while (c <= 6) 
+
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')
+// console.log('Tudo bem!')

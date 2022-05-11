74bf3934487f52b7052287aa94f30b8997146bf6
renatociqueira <37406514+renatociqueira@users.noreply.github.com>
2022-05-11 17:07:33 -0300

    Aulas de Javascript

---
+++ Aula14/ambiente.js
@@ -0,0 +1,20 @@
+// ------------------------------------------------------------------
+// Para executar o código instalar a extensão node.js Exec
+// Para utilizar a extensão apenas pressionar o F8
+// Exercicio de estrutura de repetições parte 2
+// ------------------------------------------------------------------ 
+
+/*
+var c = 1
+while( c <= 5){
+    console.log(c)
+    c++
+}
+*/
+// -----------------------
+
+console.log('Vai Começar...')
+for(var c =1; c <= 6;c++){
+    console.log(c)
+}
+console.log('fim.')
